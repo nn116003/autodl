93d5422328af731e94fb85ea46e448a6eb2e8b61
Ubuntu <ubuntu@ip-172-31-12-254.us-west-2.compute.internal>
2018-09-19 06:46:20 +0000

    add api mode

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,7 +1,43 @@
 import torchvision.datasets as datasets
+import torch.utils.data as data
 
+import pandas as pd
 
+from PIL import Image
+
+import os
 
 class ImageFolderWithPath(datasets.ImageFolder):
     def __getitem__(self, index):
         return super(ImageFolderWithPath, self).__getitem__(index), self.imgs[index][0]
+
+
+class ImageWithScore(data.Dataset):
+    def __init__(self, score_data, root='./', transform=None,
+                 target_transform=None, with_path=False):
+        self.score_data = pd.read_csv(score_data)
+        self.root = root
+        self.transform = transform
+        self.target_transform = target_transform
+        self.with_path = with_path
+        
+    def __getitem__(self, index):
+        path = self.score_data.iloc[index, 0]
+        score = self.score_data.iloc[index, 1]
+        sample = Image.open(os.path.join(self.root, path)).convert('RGB')
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(score)
+
+        if self.with_path:
+            return sample, target, path
+        else:
+            return sample, target
+
+    def __len__(self):
+        return self.score_data.shape[0]
+
+    def score_stats(self):
+        scores = self.score_data.iloc[:,1]
+        return scores.mean(), scores.std()
--- web_demo/app.py
+++ web_demo/app.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import logging
 import flask
+from flask import make_response, jsonify
 import werkzeug
 import optparse
 import tornado.wsgi
@@ -17,42 +18,24 @@ import exifutil
 
 import base64
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torchvision.transforms as transforms
-import torchvision.models as models
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
 
-from torchvision import get_image_backend
-from PIL import Image
+from models import ImageClassifier
+from web_utils import *
 
-def pil_loader(path):
-    return Image.open(path).convert('RGB')
-#    with open(path, 'rb') as f:
-#        with Image.open(f) as img:
-#            return img.convert('RGB')
 
-def default_loader(path):
-    if get_image_backend() == 'accimage':
-        return accimage_loader(path)
-    else:
-        return pil_loader(path)
-
-REPO_DIRNAME = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '/../')
-UPLOAD_FOLDER = '/tmp/demos_uploads'
-ALLOWED_IMAGE_EXTENSIONS = set(['png', 'bmp', 'jpg', 'jpe', 'jpeg', 'gif'])
+TYPE = "APP" # APP / API
 
 # Obtain the flask app object
 app = flask.Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html', has_result=False)
+    if TYPE == "APP":
+        return flask.render_template('index.html', has_result=False)
+    else:
+        return "Hello world."
 
 
 @app.route('/classify_url', methods=['GET'])
@@ -67,15 +50,24 @@ def classify_url():
         # For any exception we encounter in reading the image, we will just
         # not continue.
         logging.info('URL Image open error: %s', err)
-        return flask.render_template(
-            'index.html', has_result=True,
-            result=(False, 'Cannot open image from URL.')
-        )
+        if TYPE == "APP":
+            return flask.render_template(
+                'index.html', has_result=True,
+                result=(False, 'Cannot open image from URL.')
+            )
+        else:
+            return make_response(jsonify(ERR_DICT("img_url_err")))
 
     logging.info('Image: %s', imageurl)
     result = app.clf.classify_image(image)
-    return flask.render_template(
-        'index.html', has_result=True, result=result, imagesrc=imageurl)
+    if TYPE == "APP":
+        return flask.render_template(
+            'index.html', has_result=True, result=result, imagesrc=imageurl)
+    else:
+        if result[0]:
+            return make_response(jsonify(result[2]))
+        else:
+            return make_response(jsonify(ERR_DICT("img_classify_err")))
 
 
 @app.route('/classify_upload', methods=['POST'])
@@ -92,100 +84,31 @@ def classify_upload():
 
     except Exception as err:
         logging.info('Uploaded image open error: %s', err)
-        return flask.render_template(
-            'index.html', has_result=True,
-            result=(False, 'Cannot open uploaded image.')
-        )
+        if TYPE == "APP":
+            return flask.render_template(
+                'index.html', has_result=True,
+                result=(False, 'Cannot open uploaded image.')
+            )
+        else:
+            return make_response(jsonify(ERR_DICT["img_file_err"]))
+
 
     result = app.clf.classify_image(image)
-    return flask.render_template(
-        'index.html', has_result=True, result=result,
-        imagesrc=embed_image_html(image)
-    )
+
+    if TYPE == "APP":
+        return flask.render_template(
+            'index.html', has_result=True, result=result,
+            imagesrc=embed_image_html(image)
+        )
+    else:
+        if result[0]:
+            return make_response(jsonify(result[2]))
+        else:
+            return make_response(jsonify(ERR_DICT("img_classify_err")))
 
 
-def embed_image_html(image):
-    """Creates an image embedded in HTML base64 format."""
-    string_buf = io.BytesIO()
-    image.save(string_buf, format='png')
-    data = base64.b64encode(string_buf.getvalue()).decode("utf-8")
-    return 'data:image/png;base64,' + data
 
 
-def allowed_file(filename):
-    return (
-        '.' in filename and
-        filename.rsplit('.', 1)[1] in ALLOWED_IMAGE_EXTENSIONS
-    )
-
-
-class ImagenetClassifier(object):
-    default_args = {
-        'pretrained_model_file': (
-            '{}/model_best.pth.tar'.format(REPO_DIRNAME)),
-        'class_labels_file': (
-            '{}/lab_ref.csv'.format(REPO_DIRNAME)),
-    }
-
-
-    #default_args['arch'] = 'resnet18'
-    #default_args['class_num'] = 1000
-    
-    def __init__(self, num_classes, arch,
-                 class_labels_file, gpu_mode=True, pretrained_model_file=None):
-        logging.info('Loading net and associated files...')
-        self.net = models.__dict__[arch](num_classes = num_classes)
-
-        if gpu_mode:
-            self.net = torch.nn.DataParallel(self.net).cuda()
-        
-        if os.path.exists(pretrained_model_file):
-            checkpoint = torch.load(pretrained_model_file)
-            self.net.load_state_dict(checkpoint['state_dict'])
-
-        self.net.eval()
-            
-        labels_df = pd.read_csv(class_labels_file)
-        self.labels = labels_df.sort_values(by=["id"], ascending=True)['name'].values
-
-        self.loader = default_loader
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
-        self.preprocess = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize])
-
-    def update_model(self, pretrained_model_file=None):
-        if pretrained_model_file is not None:
-            checkpoint = torch.load(pretrained_model_file)
-            self.net.load_state_dict(checkpoint['state_dict'])
-
-    def classify_image(self, image):# image PIL.Image 0-255
-        try:
-            starttime = time.time()
-            input_img = torch.autograd.Variable(self.preprocess(image).unsqueeze(0))
-            output = F.softmax(self.net(input_img))
-            endtime = time.time()
-            
-            #score, pred = output.data.max(1)
-            scores = output.data.cpu().numpy()[0,:]
-            logging.info(str(scores))
-            indices = (-scores).argsort()
-            logging.info(str(indices))
-            use_num = min(5, len(indices))
-
-            res = [(self.labels[idx], scores[idx]) for idx in indices[:use_num]]
-            
-            logging.info('result: %s', str(res))
-
-            return (True, "meta", res, '%.3f' % (endtime - starttime))
-
-        except Exception as err:
-            logging.info('Classification error: %s', err)
-            return (False, 'Something went wrong when classifying the '
-                           'image. Maybe try another one?')
 
 
 def start_tornado(app, port=5000):
@@ -201,15 +124,10 @@ def start_from_terminal(app):
     Parse command line options and start the server.
     """
     import argparse
-    model_names = sorted(name for name in models.__dict__
-                             if name.islower() and not name.startswith("__")
-                             and callable(models.__dict__[name]))
     
     parser = argparse.ArgumentParser(description='PyTorch ImageClassification Web Demo')
     parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
-                    choices=model_names,
-                    help='model architecture: ' +
-                        ' | '.join(model_names) +
+                    help='model architecture ' +
                         ' (default: resnet18)')
     parser.add_argument('-c', '--num_classes', default=1000, type=int, metavar='N',
                     help='number of class (default: 1000)')
@@ -225,15 +143,21 @@ def start_from_terminal(app):
         '-g', '--gpu',
         help="use gpu mode",
         action='store_true', default=True)
+    parser.add_argument(
+        '-ap', '--api',
+        help="api mode",
+        action='store_true', default=False)
+
     args = parser.parse_args()
+
+    if args.api:
+        global TYPE
+        TYPE = 'API'
     
-
-
-    args = parser.parse_args()
-    ImagenetClassifier.default_args.update({'gpu_mode': args.gpu})
+    ImageClassifier.default_args.update({'gpu_mode': args.gpu})
 
     # Initialize classifier + warm start by forward for allocation
-    app.clf = ImagenetClassifier(arch = args.arch, num_classes = args.num_classes, **ImagenetClassifier.default_args)
+    app.clf = ImageClassifier(arch = args.arch, num_classes = args.num_classes, **ImageClassifier.default_args)
 #    app.clf.net.forward()
 
     if args.debug:
